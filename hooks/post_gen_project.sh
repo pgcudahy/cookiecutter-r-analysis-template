304aae2c029f8bc435eb89dfa6fe4d57796883d3
Patrick Cudahy <pgcudahy@gmail.com>
2023-11-02 10:44:45 -0400

    Add post install hooks

---
+++ hooks/post_gen_project.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Set up a reproducible R environment
+# Adapted from https://github.com/khughitt/cookiecutter-rmd-data-science
+
+# install renv
+Rscript -e "install.packages('renv', repos='http://cran.us.r-project.org')"
+
+# initialize new package-specific environment / private library;
+Rscript -e "renv::init()"
+Rscript -e "install.packages(c('tidyverse'), repos='http://cran.us.r-project.org')"
+Rscript -e "renv::snapshot(confirm = FALSE)"
+
+# initialze git repo
+git init
+git add .
+git commit -am 'Git initialized by cookiecutter'
