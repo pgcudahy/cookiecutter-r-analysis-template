11ba1b2e45b4941d29f90c2274ec725eff2de727
Patrick Cudahy <pgcudahy@gmail.com>
2024-04-04 11:24:58 -0400

    still working

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-echo "Starting post-installation script"
+echo "Starting post-installation script" >&2
 
-echo "Initialize new package-specific environment / private library"
+echo "Initialize new package-specific environment / private library" >&2
 Rscript --verbose "r/setup_renv.r"
 
-echo "Initialze git repo"
+echo "Initialze git repo" >&2
 git init
 git add .
 git commit -am 'Git initialized by cookiecutter'
--- {{cookiecutter.project_slug}}/r/setup_renv.r
+++ {{cookiecutter.project_slug}}/r/setup_renv.r
@@ -1,11 +1,14 @@
 # Install the pak package manager and renv
+sessionInfo()
+
 if(!require("pak")) install.packages("pak")
 if(!require("renv")) install.packages("renv")
 
 # Initialize new package-specific environment
 # Per https://github.com/rstudio/renv/issues/1210
 options(renv.config.pak.enabled = TRUE)
-options(renv.config.auto.snapshot = TRUE)
+#options(renv.config.auto.snapshot = TRUE)
 renv::init()
 renv::install(c('pak', 'tidyverse', 'IRkernel', 'languageserver'), prompt = FALSE)
 IRkernel::installspec()
+renv::snapshot()
