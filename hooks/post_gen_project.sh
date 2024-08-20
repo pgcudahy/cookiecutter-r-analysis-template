4b61a36e9faa69444d40d526a09af5d0dcaff22a
Patrick Cudahy <pgcudahy@gmail.com>
2024-08-20 04:06:06 -0400

    remove renv for now

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 echo "Starting post-installation script" >&2
 
-echo "Initialize new package-specific environment / private library" >&2
-Rscript --verbose "r/setup_renv.r"
+#echo "Initialize new package-specific environment / private library" >&2
+#Rscript --verbose "r/setup_renv.r"
 
 echo "Initialze git repo" >&2
 git init
