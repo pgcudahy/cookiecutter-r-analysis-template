dbfea73834570b63eea90d546dac332ad24e598d
Patrick Cudahy <pgcudahy@gmail.com>
2024-09-09 11:02:59 +0200

    start work on setting up python venv

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 echo "Starting post-installation script" >&2
 
+#Setup virtual environments for R and python
 #echo "Initialize new package-specific environment / private library" >&2
 #Rscript --verbose "r/setup_renv.r"
+#python3 -m venv "python/venv"
 
+#Setup git
 echo "Initialze git repo" >&2
 git init
 git add .
